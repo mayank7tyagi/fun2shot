53bfed7a2e7c74a5ec5a01d5b8dac3d8ba597b29
Mayank Tyagi <115171116+mayank7tyagi@users.noreply.github.com>
2023-11-16 23:08:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ for (let i = 0; i < numberOfColorBoxes; i++) {
 }
 
 
+
+
 const nav = document.querySelector( '.navbar' )
 fetch( './navbar.html')
 .then(res => res.text())
@@ -16,104 +18,41 @@ fetch( './navbar.html')
     nav.innerHTML=data;
 })
 
-    
-    
-    
-    
-    
-  
 
-    const gameData = [
-        // { 
-        // imgSrc: "game1.jpg",
-        // altText: "Game 1",
-        // name: "Game 1"
-        // },
-        // { 
-        // imgSrc: "game2.jpg",
-        // altText: "Game 2",
-        // name: "Game 2"
-        // },
-        // { 
-        //     imgSrc: "game3.jpg",
-        //     altText: "Game 3",
-        //     name: "Game 3"
-        //     },
-        // Add more game data as needed
-    ];
-    
-    // Function to create a game card element
-    function createGameCard(game) {
-        const card = document.createElement('div');
-        card.classList.add('card');
-    
-        const img = document.createElement('img');
-        img.src = game.imgSrc;
-        img.alt = game.altText;
-    
-        const title = document.createElement('h3');
-        title.textContent = game.name;
-    
-        card.appendChild(img);
-        card.appendChild(title);
-    
-        return card;
-    }
-    
-    // Function to load game cards
-    function loadGameCards() {
-        const cardContainer = document.getElementById('game-cards');
-    
-        // Load the first 2 game cards
-        for (let i = 0; i < 2; i++) {
-        if (gameData[i]) {
-            const gameCard = createGameCard(gameData[i]);
-            cardContainer.appendChild(gameCard);
+    var util = {
+        mobileMenu() {
+        $("#nav").toggleClass("nav-visible");
+        },
+        windowResize() {
+        if ($(window).width() > 800) {
+            $("#nav").removeClass("nav-visible");
         }
-        }
-    }
-    
-    // Function to load more game cards
-    function loadMoreGameCards() {
-        const cardContainer = document.getElementById('game-cards');
-        const loadMoreButton = document.getElementById('load-more-button');
-    
-        // Load the next 2 game cards
-        const startIndex = cardContainer.childElementCount;
-        for (let i = startIndex; i < startIndex + 2; i++) {
-        if (gameData[i]) {
-            const gameCard = createGameCard(gameData[i]);
-            cardContainer.appendChild(gameCard);
-        }
-        }
-    
-        // Hide the load more button if there are no more game cards
-        if (startIndex + 2 >= gameData.length) {
-        loadMoreButton.style.display = 'none';
-        }
-    }
-    
-    // Function to handle menu click events
-    function handleMenuClick(event) {
-        event.preventDefault();
-        const target = event.currentTarget.getAttribute('data-target');
-        const targetSection = document.getElementById(target);
+        },
+        scrollEvent() {
+        var scrollPosition = $(document).scrollTop();
         
-        // Scroll to the target section with smooth animation
-        targetSection.scrollIntoView({ behavior: 'smooth' });
-    }
+        $.each(util.scrollMenuIds, function(i) {
+            var link = util.scrollMenuIds[i],
+                container = $(link).attr("href"),
+                containerOffset = $(container).offset().top,
+                containerHeight = $(container).outerHeight(),
+                containerBottom = containerOffset + containerHeight;
     
-    // Function to initialize the page
-    function initializePage() {
-        const menuItems = document.querySelectorAll('#menu li a');
-        menuItems.forEach(item => {
-        item.addEventListener('click', handleMenuClick);
+            if (scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20) {
+            $(link).addClass("active");
+            } else {
+            $(link).removeClass("active");
+            }
         });
+        }
+    };
     
-        loadGameCards();
-        const loadMoreButton = document.getElementById('load-more-button');
-        loadMoreButton.addEventListener('click', loadMoreGameCards);
-    }
-    
-    // Call the initialization function when the page finishes loading
-    window.addEventListener('load', initializePage);
+    $(document).ready(function() {
+        
+        util.scrollMenuIds = $("a.nav-link[href]");
+        $("#menu").click(util.mobileMenu);
+        $(window).resize(util.windowResize);
+        $(document).scroll(util.scrollEvent);
+        
+    });
+  
